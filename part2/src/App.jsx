956dff4c9d0ea1da0c0b150d234b2e3d8f55a7bc
Anoop Lamba <anooplamba77@gmail.com>
2023-12-26 09:57:24 +0530

    added the phonebook step4

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import Phonebook from "./components/PhoneBook";
 
 function App() {
--- part2/src/components/PhoneBook.jsx
+++ part2/src/components/PhoneBook.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 
 const Phonebook = () => {
-  const [persons, setPersons] = useState([]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", phone: "040-123456", id: 1 },
+    { name: "Ada Lovelace", phone: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", phone: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", phone: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [query, setQuery] = useState("");
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
@@ -22,9 +28,27 @@ const Phonebook = () => {
     setNewNumber("");
   };
 
+  const filteredResults = persons.filter((person) =>
+    person.name.toLowerCase().includes(query.toLowerCase())
+  );
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <br />
+
+      <label>
+        Search person: &nbsp;
+        <input
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          type="text"
+        />
+      </label>
+      <br />
+      <br />
+
+      <h2>Add new person</h2>
       <br />
       <form onSubmit={handleFormSubmit}>
         <label>
@@ -49,8 +73,10 @@ const Phonebook = () => {
         <button type="submit">add</button>
       </form>
       <br />
+
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      <br />
+      {filteredResults.map((person) => (
         <p key={person.id}>
           {person.name} {person.phone}
         </p>
